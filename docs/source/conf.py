f6b03a14c5b354cdb30c71d9c5357369b3eaa2a6
goodwi11 <coolbars495@gmail.com>
2024-05-14 18:40:41 +0300

    static path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,11 +28,11 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-
 html_theme = 'sphinx_rtd_theme'
-html_css_files = [
-    'css/custom.css'
-]
+
+html_static_path = ['_static']
+html_css_files = ['css/custom.css']
+
 html_logo = 'img/logo.svg'
 html_theme_options = {
     'logo_only': True,
